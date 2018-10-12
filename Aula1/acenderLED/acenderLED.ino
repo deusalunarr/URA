661a6364d497633bfbd522da8343be73391a5662
bcq <bcq.cpp@gmail.com>
2018-10-12 17:11:19 -0300

    acender 1 led

---
+++ Aula1/acenderLED/acenderLED.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  pinMode(3, OUTPUT);  //Configura o LED na porta digital 3 como pino de SAIDA
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);  //Acende o LED
+  delay(1000);            //Mantem o LED aceso por 1000 milissegundos (1 segundo)
+  digitalWrite(3, LOW);  //Apaga o LED
+  delay(1000);           //Mantem o LED apagado por 1000 milissegundos (1 segundo)
+}
